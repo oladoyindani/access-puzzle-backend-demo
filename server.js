bf04ecd068f3132b93813095d85b3dd963610a55
Oladan-Grandpa <62756952+oladoyindani@users.noreply.github.com>
2025-02-12 08:22:12 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const bodyParser = require('body-parser');
+//const bodyParser = require('body-parser');
 const app = express();
 require('dotenv').config();
 
@@ -15,17 +15,8 @@ const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
-app.use(bodyParser.json());
+//app.use(bodyParser.json());
 app.use(express.json());
-
-// Routes
-app.get("/", (req, res) => {
-  res.send("Backend is running!");
-});
-
-app.get("/api/data", (req, res) => {
-  res.json({ message: "Hello from the backend!" });
-});
 
 // Connect to MongoDB
 mongoose
